55febf17f1ebfa6adb511ac86634fd27519870ec
Brent Barrese <brent.barrese@gmail.com>
2026-01-04 10:03:47 -0800

    Initial Commit

---
+++ TouchLine/TouchLineApp.swift
@@ -0,0 +1,32 @@
+//
+//  TouchLineApp.swift
+//  TouchLine
+//
+//  Created by Brent Barrese on 1/4/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TouchLineApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
